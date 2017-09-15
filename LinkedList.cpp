67b4aa7669875594b9ffdf61ef5cb57f65f2665a
Joel Macias <macias22@unlv.nevada.edu>
2017-09-14 17:14:03 -0700

    blank linked list class files

---
+++ LinkedList.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Joel on 9/14/17.
+//
+
+#include "LinkedList.h"
---
+++ LinkedList.h
@@ -0,0 +1,14 @@
+//
+// Created by Joel on 9/14/17.
+//
+
+#ifndef GOODOLDSTACK_LINKEDLIST_H
+#define GOODOLDSTACK_LINKEDLIST_H
+
+
+class LinkedList {
+
+};
+
+
+#endif //GOODOLDSTACK_LINKEDLIST_H
