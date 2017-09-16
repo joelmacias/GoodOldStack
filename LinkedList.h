dfa0bcd6b2fdd99df7b01bf3ae3256f7c808c8c5
Joel Macias <macias22@unlv.nevada.edu>
2017-09-15 23:09:54 -0700

    Working linked list, and main test program.

--- LinkedList.h
+++ LinkedList.h
@@ -5,10 +5,110 @@
 #ifndef GOODOLDSTACK_LINKEDLIST_H
 #define GOODOLDSTACK_LINKEDLIST_H
 
+#include<cstddef>
+#include<iostream>
 
+template <class DataType>
 class LinkedList {
+public:
+    //constructor
+    LinkedList();
+
+    //insert at head
+    void InsertAtHead(DataType data);
+
+    //remove at head
+    void RemoveFromHead();
+
+    //returns size of list
+    std::size_t GetSize();
+
+    //returns true if list empty, false other wise
+    bool Empty();
+
+    //print list
+    void Print();
+
+private:
+    struct Node{
+        DataType data;
+        Node* next;
+        Node(DataType data);
+    };
+
+    Node* head;
+    std::size_t listSize;
+
 
 };
 
+template <class DataType>
+LinkedList<DataType>::Node::Node(DataType data) {
+   this->data = data;
+   next = NULL;
+}
+
+template <class DataType>
+LinkedList<DataType>::LinkedList() {
+    head = NULL;
+    listSize = 0;
+}
+
+template <class DataType>
+void LinkedList<DataType>::InsertAtHead(DataType data) {
+    Node* newNode = new Node(data);
+
+    if(head == NULL){
+        head = newNode;
+    }
+    else{
+        newNode->next = head;
+        head = newNode;
+    }
+    listSize++;
+}
+
+template <class DataType>
+void LinkedList<DataType>::RemoveFromHead() {
+
+    if(listSize > 0) {
+        Node *temp = head;
+        head = head->next;
+        delete temp;
+        listSize--;
+    }
+    else{
+        std::cout <<"List is empty, cannot delete." << std::endl;
+    }
+}
+
+template <class DataType>
+std::size_t LinkedList<DataType>::GetSize(){
+    return listSize;
+}
+
+template <class DataType>
+bool LinkedList<DataType>::Empty() {
+    if(listSize == 0){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+template <class DataType>
+void LinkedList<DataType>::Print() {
+    if(listSize == 0){
+        std::cout << "List is empty." << std::endl;
+    }
+    else{
+        Node* temp = head;;
+        while(temp != NULL){
+            std::cout << temp->data << std::endl;
+            temp = temp->next;
+        }
+    }
+}
 
 #endif //GOODOLDSTACK_LINKEDLIST_H
--- main.cpp
+++ main.cpp
@@ -1,6 +1,30 @@
 #include <iostream>
+#include "LinkedList.h"
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+
+    LinkedList<int> list;
+
+    list.InsertAtHead(5);
+    list.InsertAtHead(4);
+    list.InsertAtHead(3);
+    list.InsertAtHead(2);
+    list.InsertAtHead(1);
+    list.InsertAtHead(0);
+
+    list.Print();
+
+
+    while(!list.Empty()){
+        list.RemoveFromHead();
+    }
+
+    if(list.Empty()){
+        std::cout << "List is empty!" << std::endl;
+    }
+    else{
+        std::cout << "List is not empty!" << std::endl;
+    }
+
     return 0;
 }
